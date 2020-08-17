8a4953cb5ee19f41dd1264d9f39130c80322d912
Renatolobo1 <renatolobo_@hotmail.com>
2020-08-17 14:16:18 -0300

    Configurando Nodemon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const express =require('express');
 const app = express();
 
 app.get('/', (request, response) => { 
-  return response.json({message: 'Heloo Lôbo!'});
+  return response.json({message: 'Heloo Lôbo!!!'});
 });
 
-app.listen(3333);
+app.listen(3333, ()=> {
+  console.log('Back-end started!!')
+});
